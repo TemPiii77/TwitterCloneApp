6d703fdc0f2c380c78b362694470f1d2469b0514
Toth-Andor Kristof <t.a.kristof0210@gmail.com>
2024-07-20 16:01:24 +0100

    Creating Frontend and Backend and connecting to the database

---
+++ TwitterCloneBE/src/main/java/org/example/twitterclonebe/TwitterCloneBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.twitterclonebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterCloneBeApplication.class, args);
+    }
+
+}
---
+++ TwitterCloneBE/src/test/java/org/example/twitterclonebe/TwitterCloneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.twitterclonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
